535af4ce800df0ab17063bbb225a4468e48d30ee
GitGreg228 <shutov.gm@phystech.edu>
2022-05-05 13:51:10 +0300

    typo in comments

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ k_Ry_THz = 3289.84367720502  # 1 Ry = 3289.84367720502 THz
 k_Ry_K = 157887.6061  # 1 Ry = 157887.6061 K
 k_Ry_eV = 13.6056980659  # 1 Ry = 13.6056980659 eV
 k_K_THz = 0.0208366176361328  # 1 K = 0.0208366176361328 THz
-k_bohr_A = 0.529177249  # 1 bohr = 1 Angstrom
+k_bohr_A = 0.529177249  # 1 bohr = 0.529177249 Angstrom
 k_B = 1.380649 / (10 ** 23)  # J / K Boltzmann constant
 h = 6.626070e-34  # J * s Plank constant
 k_THz_Hz = 10e+12  # 1 THz = 10e+12 Hz
