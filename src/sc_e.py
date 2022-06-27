c99c4c9b22057ae1126a647ca956a1fb2473afe1
GitGreg228 <shutov.gm@phystech.edu>
2022-06-27 22:16:49 +0300

    added normal sgd

--- src/sc_e.py
+++ src/sc_e.py
@@ -53,46 +53,44 @@ class Superconducting(object):
         self.dim = dim
         t = t_min
         b = 100
-        a = 10
-        while b > 0:
-            s = np.zeros(dim + 1, dtype=np.float32)
-            # print(s)
-            for y in range(1, dim + 2):
-                s1 = 0
-                for r in range(1, y + 1):
-                    # print(y, r)
-                    s1 = s1 + l(self.a2f, r, t)
-                s[y - 1] = s1
-            # print(s)
-            d = 0
-            k = np.zeros((dim + 1, dim + 1), dtype=np.float32)
-            for n in range(1, dim + 2):
-                for m in range(1, dim + 2):
-                    if n == m:
-                        if m == 1:
-                            d = 2 * (m - 1) + 1 + l(self.a2f, 0, t)
+        dt = 10
+        while dt > 0.001:
+            while b > 0:
+                s = np.zeros(dim + 1, dtype=np.float32)
+                # print(s)
+                for y in range(1, dim + 2):
+                    s1 = 0
+                    for r in range(1, y + 1):
+                        # print(y, r)
+                        s1 = s1 + l(self.a2f, r, t)
+                    s[y - 1] = s1
+                # print(s)
+                d = 0
+                k = np.zeros((dim + 1, dim + 1), dtype=np.float32)
+                for n in range(1, dim + 2):
+                    for m in range(1, dim + 2):
+                        if n == m:
+                            if m == 1:
+                                d = 2 * (m - 1) + 1 + l(self.a2f, 0, t)
+                            else:
+                                d = 2 * (m - 1) + 1 + l(self.a2f, 0, t) + 2 * s[m - 2]
                         else:
-                            d = 2 * (m - 1) + 1 + l(self.a2f, 0, t) + 2 * s[m - 2]
-                    else:
-                        d = 0
-                    k[n - 1, m - 1] = l(self.a2f, m - n, t) + l(self.a2f, m + n - 1, t) - 2 * mu - d
-            # print(k)
-            # print(k)
-            w, _ = np.linalg.eig(k)
-            b = np.max(w)
-            self.t.append(t)
-            self.b.append(b)
-            self.k.append(k)
-            dt = a * b
-            if dt > 10:
-                # a = 0.1 * a
-                dt = 10
+                            d = 0
+                        k[n - 1, m - 1] = l(self.a2f, m - n, t) + l(self.a2f, m + n - 1, t) - 2 * mu - d
+                w, _ = np.linalg.eig(k)
+                b = np.max(w)
+                self.t.append(t)
+                self.b.append(b)
+                self.k.append(k)
+                t = t + dt
+            # print(t, dt)
+            t = t - dt
+            if dt > 0.001:
+                dt = dt * 0.1
             if dt < 0.001:
                 dt = 0.001
-                # a = 10 * a
-            t = t + dt
         self.tc_e = t - dt
-        print(f'Eliashberg Tc = {stround(t - dt)}+-{stround(dt)} K')
+        print(f'Eliashberg Tc = {stround(t)}+-{stround(dt)} K')
         return t
 
     def get_all(self, system, nef, structure):
